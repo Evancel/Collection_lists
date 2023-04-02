6cd6a9dea660df56dc5637e71a71234254dc1244
Evancel <w.lexandra@gmail.com>
2023-04-02 23:18:42 +0300

    initial commit

---
+++ src/main/java/pro/sky/collection_l1/CollectionL1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.collection_l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionL1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionL1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/collection_l1/CollectionL1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collection_l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionL1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
